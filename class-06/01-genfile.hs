aa8eb26c10512ecaf56166f0866f532ca0e72165
bopre <89094191707@mail.ru>
2014-10-21 19:24:19 +0400

    Update 01-genfile.hs

--- class-06/01-genfile.hs
+++ class-06/01-genfile.hs
@@ -9,9 +9,16 @@
 -}
 import System.Environment
 
-createFile :: Int -> String -> FilePath -> IO ()
-createFile n s fname = undefined
 
+{-
+writeFile "file.out" (operate src)
+-}
+
+createFile :: Int -> String -> FilePath -> IO ()
+createFile n s fname = writeFile fname (repeatNStr 1)
+	where repeatNStr i
+		|i==n=s
+		|otherwise=s++('\n':(repeatNStr (i+1)))
 main = do
   [n_str, text, fname] <- getArgs
   createFile (read n_str) text fname
