f18afb3fc76f9516fdb1065658f99981a5de6971
bopre <89094191707@mail.ru>
2014-10-21 19:23:42 +0400

    Update 02-textproc.hs

--- class-06/02-textproc.hs
+++ class-06/02-textproc.hs
@@ -13,4 +13,57 @@
   командной строки.
 -}
 
-main = undefined
+import System.Environment
+import System.Directory
+import Data.Char
+
+
+
+cntStr :: String -> Integer
+cntStr []=0
+cntStr (x:xs)
+	|x=='\n'=1+cntStr xs
+	|otherwise=cntStr xs
+
+strToUpper :: String -> String
+strToUpper [] = []
+strToUpper (x:xs) = (toUpper x):(strToUpper xs)
+
+countStrInFile :: String -> Integer
+countStrInFile src = if (src==[]) then 0 else (cntStr src) +1 
+
+addToEnd :: String -> FilePath -> IO ()
+addToEnd str fname = appendFile fname ('\n':str)
+
+
+
+addToStart :: String -> FilePath -> IO ()
+addToStart str fname = do
+	str1 <- readFile fname
+	writeFile ("temp111"++fname) (str++['\n'])
+	appendFile ("temp111"++fname) str1
+	str1 <- readFile ("temp111"++fname)
+	writeFile fname str1
+	removeFile ("temp111"++fname)
+
+printFileToUpper :: FilePath -> IO ()
+printFileToUpper fname = do
+	str1 <- readFile fname
+	let str2 = strToUpper str1
+	appendFile ("temp111"++fname) str2
+	str1 <- readFile ("temp111"++fname)
+	writeFile fname str1
+	removeFile ("temp111"++fname)
+
+concatinateFiles fname1 fname2 = undefined
+--intToDigit :: Int -> Char
+
+cccddd = chr
+
+generateRandomFile col row = undefined
+
+main = do
+  [fname] <- getArgs
+  printFileToUpper fname
+
+
