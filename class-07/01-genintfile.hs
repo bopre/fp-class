ed627ed0569c31d483c69f231089fb65f348a17c
bopre <89094191707@mail.ru>
2014-10-27 17:21:53 +0300

    Update 01-genintfile.hs

--- class-07/01-genintfile.hs
+++ class-07/01-genintfile.hs
@@ -6,5 +6,34 @@
   3) количество чисел в строке;
   4) количество строк в файле.
 -}
+import System.Random
+import System.Environment
 
-main = undefined
+createRandomStr :: Int -> Int -> Int -> StdGen -> (String, StdGen)
+createRandomStr _ _ 0 gen = ("",gen)
+createRandomStr minEl maxEl 1 gen = let (x, newGen) = randomR(minEl,maxEl) gen :: (Int, StdGen)
+					    in ((show x), newGen)
+createRandomStr minEl maxEl n gen = let (x, newGen) = randomR(minEl,maxEl) gen :: (Int, StdGen)
+				        (tale, newGen') = (createRandomStr minEl maxEl (n-1) newGen)
+					 in ((((show x)++", ")++tale), newGen')
+
+
+createRandomText :: Int -> Int -> Int -> Int -> StdGen -> [String]
+createRandomText _ _ _ 0 gen = []
+createRandomText minEl maxEl col 1 gen = let (str, newGen) = createRandomStr minEl maxEl col gen
+					     in [str]
+createRandomText minEl maxEl col n gen = let (str, newGen) = createRandomStr minEl maxEl col gen
+					     in str:(createRandomText minEl maxEl col (n-1) newGen)
+
+createRandomFile :: String -> Int -> Int -> Int -> Int -> IO ()
+createRandomFile fname minElem maxElem col row = do
+	gen <- newStdGen
+	let int_lines = createRandomText minElem maxElem col row gen
+	writeFile fname (unlines int_lines)
+
+
+
+main = do
+	[fname, minElem, maxElem, col, row] <- getArgs
+	createRandomFile fname (read minElem) (read maxElem) (read col) (read row)
+
