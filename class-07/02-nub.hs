8656d46c1c5f64aa3742869e3d5d5845ea0bad6c
bopre <89094191707@mail.ru>
2014-10-28 01:30:39 +0300

    Update 02-nub.hs

--- class-07/02-nub.hs
+++ class-07/02-nub.hs
@@ -22,16 +22,36 @@ nub_seq :: Seq.Seq a -> Int
 nub_seq = undefined
 
 nub_arr :: Array Int Int -> Int
-nub_arr = undefined
+nub_arr arr = 1000 - (count0InArray 1000 (throughArray 1000 showarr1 arr))
+
+throughArray :: Int -> Array Int Int -> Array Int Int -> Array Int Int
+throughArray 0 arr1 arr2 = arr1
+throughArray n arr1 arr2 = throughArray (n-1) arr1 (incIElem (arr1 ! n) arr2)
+
+showarr1 :: Array Int Int
+showarr1 = array (1,1000) [(i,0) | i <- [1..1000]]
+
+incIElem :: Int -> Array Int Int -> Array Int Int
+incIElem n arr = array (1,1000) [(i,(if(i==n) then ((arr ! i)+1) else (arr ! i))) | i <- [1..1000]]
+
+recv1 :: Int
+recv1 = showarr1 ! 24
+
+count0InArray :: Int -> Array Int Int -> Int
+count0InArray 0 _ = 0
+count0InArray n arr = (count0InArray (n-1) arr)+(if (arr ! n)==0 then 1 else 0)
+
 
 main = do
   [fname] <- getArgs
   content <- readFile fname
   let xs = map read $ concatMap words $ lines content
-  let (n:results) = [
+  let (n:results) = [{-
         nub_set $ Set.fromList xs,
         nub_list xs,
-        nub_seq $ Seq.fromList xs,
+        nub_seq $ Seq.fromList xs,-}
         nub_arr $ listArray (1,length xs) xs ]
   mapM_ print results
+  let x = nub_arr $ listArray (1,length xs) xs
+  print x
   when (any (/= n) results) $ putStrLn "Результаты не совпадают!"
