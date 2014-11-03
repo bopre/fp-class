a544854a5b7abb156e95868c6fb7f6f98f46ff4b
bopre <89094191707@mail.ru>
2014-11-03 15:22:18 +0300

    Update main.hs

--- class-08/03-queues/main.hs
+++ class-08/03-queues/main.hs
@@ -9,16 +9,33 @@ checkQueue q = lastElem (enqueue q 5) == 5
  where
   lastElem q = let (x, q') = dequeue q in
                if isEmpty q' then x else lastElem q'
-{-
-main = print $
-         checkQueue (enqueue empty 10 :: Q.Queue Int)
-         &&  checkQueue (enqueue empty 10 :: FQ.Queue Int)
--}
+
+queueAreSimilar :: (AbstractQueue q1, AbstractQueue q2, Num a, Eq a) => q1 a -> q2 a -> Bool
+queueAreSimilar q1 q2 = if (isEmpty q1)/=(isEmpty q2) then False else if (isEmpty q1) then True else 
+				let (x1,q1') = dequeue q1
+				    (x2,q2') = dequeue q2
+					in ((x1==x2) && (queueAreSimilar q1' q2'))
+
+main = do
+	[nS] <- getArgs
+	let n = read nS
+	gen <- newStdGen
+	let x=createRandomList n gen
+	putStrLn $ "Generated List:"
+	putStrLn $ show x
+	putStrLn $ "----------------"
+	qNewQ <- forNdo n 0 (empty :: Q.Queue Int) x
+	qNewFQ <- forNdo n 0 (empty :: FQ.Queue Int) x
+	putStrLn $ show (queueAreSimilar qNewQ qNewFQ)
+	--putStrLn $ show ((cntElemsInQueue qNewQ)==(cntElemsInQueue qNewFQ))
+
 
 createRandomList ::  Int -> StdGen -> [Int]
 createRandomList 0 _ = []
 createRandomList n gen = let (x,newGen)=randomR(-100,100) gen :: (Int, StdGen)
 				in x:(createRandomList (n-1) newGen)
+
+cntElemsInQueue q = if isEmpty q then 0 else let (x,q')=dequeue q in (1+(cntElemsInQueue q'))
 
 printQueue q = 
 	if (isEmpty q) then return ()
@@ -26,16 +43,16 @@ printQueue q =
 		let (x, q') = dequeue q
 		putStr $ show x ++ ", "
 		printQueue q'
-
+{-
 main = do
 	[nS] <- getArgs
 	gen <- newStdGen
 	let x=createRandomList (read nS) gen
-	putStrLn $ show x
-	putStrLn $ "----------------"
+	--putStrLn $ show x
+	--putStrLn $ "----------------"
 	qNew <- forNdo (read nS) 0 (empty :: Q.Queue Int) x
 	printQueue qNew
-
+-}
 getIInList _ [] = error "end of bound"
 getIInList 1 (x:xs) = x
 getIInList n (x:xs) = getIInList (n-1) xs
@@ -47,18 +64,18 @@ oneIterationEnq n curN q l = do
 	if (n<curN) then return q
 		else do
 		let q' = enqueue q (getIInList n l)
-		putStrLn $ ""
-		printQueue q'
-		putStrLn $ "-----enq---------------"
+		--putStrLn $ ""
+		--printQueue q'
+		--putStrLn $ "-----enq---------------"
 		oneIterationEnq n (curN+1) q' l
 
 oneIterationDeq n curN q l = do
 	if (n<curN) then return q
 		else do
 		let (x, q') = dequeue q
-		putStrLn $ ""
-		printQueue q'
-		putStrLn $ "--------qec------------"
+		--putStrLn $ ""
+		--printQueue q'
+		--putStrLn $ "--------qec------------"
 		oneIterationDeq n (curN+1) q' l
 
 
@@ -67,8 +84,8 @@ forNdo n curN q l = do
 		else do
 		q' <- oneIterationEnq (curN+1) 1 q l
 		q'' <- oneIterationDeq curN 1 q' l
-		putStrLn $ ""
-		printQueue q''
-		putStrLn $ "------iter--------------"
+		--putStrLn $ ""
+		--printQueue q''
+		--putStrLn $ "------iter--------------"
 		forNdo n (curN+1) q'' l
 
