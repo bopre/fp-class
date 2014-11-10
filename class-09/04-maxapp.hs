f67b128ede89a80b2d80c66ce67f450017dc52cb
bopre <89094191707@mail.ru>
2014-11-11 00:20:27 +0300

    Update 04-maxapp.hs

--- class-09/04-maxapp.hs
+++ class-09/04-maxapp.hs
@@ -1,4 +1,5 @@
 import Control.Applicative
+import System.Environment
 
 {-
   Пользуясь возможностями аппликативных функторов, определите функцию, 
@@ -8,25 +9,33 @@ import Control.Applicative
 -}
 
 maxApp2 :: (Ord a, Applicative f) => f a -> f a -> f a
-maxApp2 = undefined
+maxApp2 a1 a2 = (\x y -> if (x>y) then x else y) <$> a1 <*> a2
 
 {- Реализуйте аналогичную функцию в случае трёх заданных значений в контексте. -}
 
 maxApp3 :: (Ord a, Applicative f) => f a -> f a -> f a -> f a
-maxApp3 = undefined
+maxApp3 a1 a2 a3 = maxApp2 a1 (maxApp2 a2 a3)
 
+{-
+maxApp3 :: (Ord a, Applicative f) => f a -> f a -> f a -> f a
+maxApp3 a1 a2 a3 = (\x y -> if (x>y) then x else y) <$> ((\x y -> if (x>y) then x else y) <$> a1 <*> a2) <*> a3
+-}
 {- Реализуйте аналогичную функцию в случае списка значений в контексте. -}
 
 maxApp :: (Ord a, Applicative f) => [f a] -> f a
-maxApp = undefined
+maxApp [x] = x
+maxApp (x:xs) = max <$> x <*> maxApp xs
 
 {-
   Продемонстрируйте использование написанных функций для аппликативных функторов Maybe,
   список (для каждого из двух экземпляров), Either String и IO.
 -}
 
-main = undefined
-
+{-
+main = do
+	x <- maxApp3 (getLine) (getLine) (getLine)
+	putStrLn x
+-}
 {- (необязательно)
   Ясно ли вам, что вы реализовали нечто, похожее на моноид на аппликативных функторах?
   Можете ли вы выразить это в коде? Необходимо ли добавлять какие-нибудь ограничения?
