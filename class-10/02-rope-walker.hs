5311b88c1a4ee68f37f1f405db238e1c6e40731b
bopre <89094191707@mail.ru>
2014-11-17 23:37:41 +0300

    Update 02-rope-walker.hs

--- class-10/02-rope-walker.hs
+++ class-10/02-rope-walker.hs
@@ -24,11 +24,11 @@ import System.Environment
 
 data MoveSt = MoveSt {typeMove :: Char, value :: Int}
 
-moveSt :: MoveSt -> Pole -> Maybe Pole
+moveSt :: MoveSt -> Pole -> Either Pole String
 moveSt p (left, right)
 	|(typeMove p)=='R'=landRight (value p) (left, right)
 	|(typeMove p)=='L'=landLeft (value p) (left, right)
-	|otherwise = Nothing
+	|otherwise = Right "That was banana..."
 
 readMoveSt :: String -> MoveSt
 readMoveSt str = let (t:tail)=(words str) in (if (head t) /= 'B' then let (v:tail2)=tail in (MoveSt (head t) (read v)) else (MoveSt (head t) (0)) )
@@ -39,11 +39,17 @@ readMoves str = map readMoveSt (lines str)
 readMovesFromFile :: FilePath -> IO [MoveSt]
 readMovesFromFile fname = readFile fname >>= (return . (map (\x -> readMoveSt x) ) . lines)
 
-runMoves :: [MoveSt] -> Maybe Pole
-runMoves [] = Nothing
-runMoves l = runThis l (0, 0)
-	where runThis [x] p = moveSt x p
-	      runThis (x:xs) p = (moveSt x p) >>= runThis xs
+runMoves :: [MoveSt] -> Either Pole String
+runMoves [] = Right "There is no move"
+--runMoves l = foldr (\x p = ) (Left (0, 0)) l
+
+runMoves l = runThis l (Left (0, 0))
+	where --runThis :: [MoveSt] -> Either Pole String -> Either Pole String
+	      runThis _ (Right s) = Right s
+	      runThis [x] (Left p) = moveSt x p
+	      runThis (x:xs) (Left p) = runThis xs (moveSt x p)
+
+
 
 main = readMovesFromFile "birds.txt" >>= return . runMoves
 
@@ -54,23 +60,26 @@ type Pole = (Birds, Birds)
 
 balance = 3
 
-updatePole :: Pole -> Maybe Pole
-updatePole p = if unbalanced p then Nothing else Just p
+updatePole :: Pole -> Either Pole String
+updatePole p = if unbalanced p then Right "disbalanced" else Left p
   where
     unbalanced (l, r) = abs (l - r) > balance
 
-landLeft :: Birds -> Pole -> Maybe Pole
+landLeft :: Birds -> Pole -> Either Pole String
 landLeft n (left, right) = updatePole (left + n, right)
 
-landRight :: Birds -> Pole -> Maybe Pole
+landRight :: Birds -> Pole -> Either Pole String
 landRight n (left, right) = updatePole (left, right + n)
 
-banana :: Pole -> Maybe Pole
-banana = const Nothing
 
+--banana :: Pole -> Either Pole String
+--banana = Right "That was banana..."
+
+{-
 tests = all test [1..3]
   where
     test 1 = (return (0, 0) >>= landLeft 1 >>= landRight 4 
-              >>= landLeft (-1) >>= landRight (-2)) == Nothing
+              >>= landLeft (-1) >>= landRight (-2)) == Right
     test 2 = (return (0, 0) >>= landRight 2 >>= landLeft 2 >>= landRight 2) == Just (2, 4)
     test 3 = (return (0, 0) >>= landLeft 1 >>= banana >>= landRight 1) == Nothing
+-}
