5d19e32ef08c74a596473596827fd21e7b0fc0cb
bopre <89094191707@mail.ru>
2014-11-29 17:52:11 +0300

    Update 01-password.hs

--- class-12/01-password.hs
+++ class-12/01-password.hs
@@ -11,25 +11,28 @@
 import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
+import System.Environment
 
 import Data.Char
 
-isValid :: String -> Bool
-isValid s = length s >= 8 && 
-                any isAlpha s && 
-                any isNumber s && 
-                any isPunctuation s
+type PasswordParams = (Int, Bool, Bool, Bool)
 
-getValidPassword :: MaybeT IO String
-getValidPassword = do
+isValid :: PasswordParams -> String -> Bool
+isValid (len, letr, numbr, punct) s = length s >= len && 
+				((any isAlpha s)||(not letr)) && 
+				((any isNumber s)||(not numbr)) && 
+				((any isPunctuation s)||(not punct))
+
+getValidPassword :: PasswordParams ->  MaybeT IO String
+getValidPassword params = do
   lift $ putStrLn "Введите новый пароль:"
   s <- lift getLine
-  guard (isValid s)
+  guard (isValid params s)
   return s
  
-askPassword :: MaybeT IO ()
-askPassword = do
-  value <- msum $ repeat getValidPassword
+askPassword :: PasswordParams -> MaybeT IO ()
+askPassword params = do
+  value <- msum $ (repeat (getValidPassword params))
   lift $ putStrLn "Сохранение в базе данных..."
 
-main = runMaybeT askPassword
+main = getArgs >>= \(x:y:z:t:tail) -> (runMaybeT (askPassword ((read x), (read y), (read z), (read t))))
