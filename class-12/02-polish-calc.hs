7c7abf684c1c560a91cb000ed39099d936b949fe
bopre <89094191707@mail.ru>
2014-12-02 00:26:24 +0300

    Update 02-polish-calc.hs

--- class-12/02-polish-calc.hs
+++ class-12/02-polish-calc.hs
@@ -25,17 +25,39 @@
 
 import Control.Monad
 import Control.Monad.State
+import Control.Monad.Trans.Maybe
 
 type Stack = [Int]
 
 push :: Int -> State Stack ()
 push x = get >>= put . (x:)
 
+isOper :: String -> Bool
+isOper x = (x=="*")||(x=="/")||(x=="+")||(x=="-")
+
+isNumber :: String -> Bool
+isNumber x = all (\z->(z>='0')&&(z<='9')) x
+
+cntOp :: String -> (Int,String)
+cntOp str = step (words str)
+	where step [] = (0,"")
+	      step (x:xs) = if(isOper x) then let (y,t)=(step xs) in ((1+y),t) else (0,(foldl (\x1 t1 -> x1 ++ " " ++ t1) x xs))
+
+cntNumbers :: String -> (Int, Bool)
+cntNumbers str = step (words str)
+	where step [] = (0,True)
+	      step (x:xs)= if(isNumber x) then let (y,b)=(step xs) in ((1+y),b) else (0,False)
+
+isGoodRecord :: String -> Maybe String
+isGoodRecord str = let (x,t) = (cntOp str) in let (y,b) = (cntNumbers t) in if (b)&&((x+1)==y) then (Just str) else Nothing
+
 pop :: State Stack Int
 pop = get >>= \(x:xs) -> put xs >> return x
 
-evalRPN :: String -> Int
-evalRPN xs = head $ execState (mapM step $ words xs) []
+evalRPN :: String -> Maybe Int
+evalRPN xs = case (isGoodRecord xs) of
+					Nothing -> Nothing
+					Just _ -> Just (head $ execState (mapM step $ words xs) [])
   where
     step "+" = processTops (+)
     step "*" = processTops (*)
